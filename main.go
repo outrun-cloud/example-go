e1e21682564efecccd7e3c780fdf499eca472561
Adrien <adrien@arccoza.com>
2022-07-30 19:19:12 +0800

    Update main

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 )
+
 
 func main() {
 	fmt.Println("Hello world!")
@@ -13,4 +15,12 @@ func main() {
 	if err := ioutil.WriteFile("./helloworld.txt", helloWorldBytes, 0644); err != nil {
 		panic(err)
 	}
+
+	fmt.Println("http://localhost:8090")
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8090", nil)
+}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
 }
